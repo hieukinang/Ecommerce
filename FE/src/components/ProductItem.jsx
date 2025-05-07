233f4ec5a2a09a6bb6850b928cc1b144abd21c4d
HieuKyNang <bmdinhhieu@gmail.com>
2025-05-08 03:21:29 +0700

    fix product

--- FE/src/components/ProductItem.jsx
+++ FE/src/components/ProductItem.jsx
@@ -2,11 +2,11 @@ import React, { useContext } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { Link } from 'react-router-dom'
 
-const ProductItem = ({ id, images, name, price }) => {
+const ProductItem = ({ id, image, name, price }) => {
   const { currency } = useContext(ShopContext);
 
   // Lấy ảnh đầu tiên từ object image
-  const displayImage = Object.values(images || {})[0] || 'fallback.jpg';
+  const displayImage = Object.values(image || {})[0] || 'fallback.jpg';
 
   return (
     <div>
--- FE/src/pages/Product.jsx
+++ FE/src/pages/Product.jsx
@@ -9,14 +9,14 @@ const Product = () => {
   const { productId } = useParams();
   const { products, currency, addToCart } = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
-  const [images, setImages] = useState('');
+  const [image, setImage] = useState('');
   const [size, setSize] = useState('');
 
   const fetchProductData = async () => {
     products.map((item) => {
       if (item._id === productId) {
         setProductData(item);
-        setImages(item.images[0]);
+        setImage(item.image[0]);
 
         return null;
       }
@@ -35,13 +35,13 @@ const Product = () => {
         <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
           <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
             {
-              productData.images.map((item, index) => (
-                <img onClick={() => setImages(item)} src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' />
+              productData.image.map((item, index) => (
+                <img onClick={() => setImage(item)} src={item} key={index} className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' />
               ))
             }
           </div>
           <div className='w-full sm:w-[80%]'>
-            <img className='w-full h-auto' src={images} alt='' />
+            <img className='w-full h-auto' src={image} alt='' />
           </div>
         </div>
 
