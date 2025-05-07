0bd4de1b58342156639970bee7d71510d5832c9e
HieuKyNang <bmdinhhieu@gmail.com>
2025-05-08 03:53:50 +0700

    fix product 2

--- FE/src/pages/Collection.jsx
+++ FE/src/pages/Collection.jsx
@@ -180,7 +180,7 @@ const Collection = () => {
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap gay-y-6'>
           {
             filterProducts.map((item, index) => (
-              <ProductItem key={index} name={item.name} id={item._id} price={item.price} image={item.images} />
+              <ProductItem key={index} name={item.name} id={item._id} price={item.price} images={item.images} />
             ))
           }
         </div>
