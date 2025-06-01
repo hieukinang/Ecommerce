c67e5353909d8bfdfb63babbe635613346ef8f31
Blqiu219 <nguyenththu219@gmail.com>
2025-06-01 09:06:33 +0700

    Fix hero

--- FE/src/components/Hero.jsx
+++ FE/src/components/Hero.jsx
@@ -4,16 +4,16 @@ import { NavLink } from 'react-router-dom'
 
 const Hero = () => {
   return (
-    <div className="relative w-full flex justify-center">
-      {/* Ảnh nền */}
+    <div className="relative w-full">
+      {/* Ảnh nền full màn hình */}
       <img
-        className="w-full max-w-[1200px] h-auto object-cover"
+        className="w-full h-auto object-cover"
         src={assets.banerrealfinal}
         alt="bannerrealfinal"
       />
 
-      {/* Nội dung nằm trên ảnh */}
-      <div className="absolute top-0 right-0 flex flex-col items-end justify-center text-white bg-black bg-opacity-30 w-full max-w-[1200px] h-full pr-16">
+      {/* Overlay nội dung phủ ảnh */}
+      <div className="absolute inset-0 flex flex-col items-end justify-center text-white  pr-16 translate-x-[-2cm]">
         <div className="flex items-center justify-center gap-2 mb-2 mt-8">
           <p className="w-8 md:w-11 h-[2px] bg-white"></p>
           <p className="font-bold text-sm md:text-base">
@@ -32,14 +32,14 @@ const Hero = () => {
         </div>
 
         <NavLink
-          to="/collection"
+          // to="/collection"
           className="inline-block mt-4 px-6 py-2 border border-white text-sm font-medium tracking-wide transition duration-300 text-white hover:bg-white hover:text-black"
+          style={{ marginLeft: '4cm' }}
         >
           SHOP NOW &gt;
         </NavLink>
       </div>
     </div>
-  )
-}
-
-export default Hero
+  );
+};
+export default Hero;
