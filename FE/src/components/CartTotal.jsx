db88cf886621b2db0a7a97ef9dbe2db8ee0aef47
Nguyễn Thị Hoài Thu <nguyenththu219@gmail.com>
2025-06-07 01:42:28 +0700

    Add Vietnamese

--- FE/src/components/CartTotal.jsx
+++ FE/src/components/CartTotal.jsx
@@ -11,26 +11,25 @@ const CartTotal = () => {
     <div className='w-full'>
 
         <div className='text-2xl'>
-            <Title text1={'CART'} text2={'TOTAL'}/>
+            <Title text1={'CHI TIẾT'} text2={'THANH TOÁN'}/>
         </div>
 
         <div className='flex flex-col gap-2 mt-2 text-sm'>
             <div className='flex justify-between'>
-                <p>SubTotal</p>
+                <p>Tổng tiền hàng</p>
                 <p>{currency} {getCartAmount()}</p>
             </div>
             <hr />
             <div className='flex justify-between'>
-                <p>Shipping Fee</p>
+                <p>Tổng tiền phí vận chuyển </p>
                 <p>{currency} {delivery_fee}</p>
             </div>
             <hr />
             <div className='flex justify-between'>
-                <b>Total</b>
+                <b>Tổng thanh toán</b>
                 <b>{currency} {getCartAmount() === 0 ? 0 : getCartAmount() + delivery_fee}</b>
             </div>
         </div>
-      
     </div>
   )
 }
