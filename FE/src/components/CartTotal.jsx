3825e7dc7bb9464bdcbaa8380dcff10e81062629
Nguyễn Thị Hoài Thu <nguyenththu219@gmail.com>
2025-06-07 09:59:47 +0700

    Add Vietnamese

--- FE/src/components/CartTotal.jsx
+++ FE/src/components/CartTotal.jsx
@@ -3,35 +3,36 @@ import { ShopContext } from '../context/ShopContext'
 import Title from './Title';
 
 const CartTotal = () => {
+  const { currency, delivery_fee, getCartAmount } = useContext(ShopContext);
 
-    const {currency, delivery_fee, getCartAmount} = useContext(ShopContext);
-
+  const formatCurrency = (amount) => {
+    return new Intl.NumberFormat('vi-VN').format(amount);
+  };
 
   return (
     <div className='w-full'>
+      <div className='text-2xl'>
+        <Title text1={'CHI TIẾT'} text2={'THANH TOÁN'} />
+      </div>
 
-        <div className='text-2xl'>
-            <Title text1={'CHI TIẾT'} text2={'THANH TOÁN'}/>
+      <div className='flex flex-col gap-2 mt-2 text-sm'>
+        <div className='flex justify-between'>
+          <p>Tổng tiền hàng</p>
+          <p>{formatCurrency(getCartAmount())} {currency}</p>
         </div>
-
-        <div className='flex flex-col gap-2 mt-2 text-sm'>
-            <div className='flex justify-between'>
-                <p>Tổng tiền hàng</p>
-                <p>{currency} {getCartAmount()}</p>
-            </div>
-            <hr />
-            <div className='flex justify-between'>
-                <p>Tổng tiền phí vận chuyển </p>
-                <p>{currency} {delivery_fee}</p>
-            </div>
-            <hr />
-            <div className='flex justify-between'>
-                <b>Tổng thanh toán</b>
-                <b>{currency} {getCartAmount() === 0 ? 0 : getCartAmount() + delivery_fee}</b>
-            </div>
+        <hr />
+        <div className='flex justify-between'>
+          <p>Tổng tiền phí vận chuyển </p>
+          <p>{formatCurrency(delivery_fee)} {currency}</p>
         </div>
+        <hr />
+        <div className='flex justify-between'>
+          <b>Tổng thanh toán</b>
+          <b>{formatCurrency(getCartAmount() === 0 ? 0 : getCartAmount() + delivery_fee)} {currency}</b>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default CartTotal
+export default CartTotal;
--- FE/src/pages/Product.jsx
+++ FE/src/pages/Product.jsx
@@ -34,9 +34,9 @@ const Product = () => {
 
   return productData ? (
     <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
-      {/** Product Data */}
+      {/* Product Data */}
       <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
-        {/** Product Image */}
+        {/* Product Image */}
         <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
           <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
             {
@@ -53,23 +53,14 @@ const Product = () => {
         {/* Product Information */}
         <div className='flex-1'>
           <h1 className='font-medium text-2xl mt-2'>{productData.name}</h1>
-          {/* <div className='flex items-center gap-1 mt-2'>
-            <img src={assets.star_icon} alt='' className='w-3 5' />
-            <img src={assets.star_icon} alt='' className='w-3 5' />
-            <img src={assets.star_icon} alt='' className='w-3 5' />
-            <img src={assets.star_icon} alt='' className='w-3 5' />
-            <img src={assets.star_dull_icon} alt='' className='w-3 5' />
-            <p className='pl-2'>(122)</p>
-          </div> */}
-          <p className='mt-5 text-3xl font-medium'>{new Intl.NumberFormat('vi-VN').format(productData.price)} {currency}</p>
-          
-          
+
+          <p className='mt-5 text-3xl font-medium'>
+            {new Intl.NumberFormat('vi-VN').format(productData.price)} {currency}
+          </p>
+
           <div className='flex items-center gap-4 mt-6'>
             {/* Ô số lượng */}
             <div className='flex items-center border rounded-lg overflow-hidden'>
-              {/* <label htmlFor='quantity' className='px-3 text-sm text-gray-600 bg-gray-100 border-r'>
-                Qty
-              </label> */}
               <input
                 type='number'
                 id='quantity'
@@ -88,9 +79,14 @@ const Product = () => {
             </button>
           </div>
 
+          {/* Mô tả sản phẩm */}
+          <div className='mt-8 text-sm text-gray-700 leading-relaxed  p-4 rounded-lg'>
+            <b className='block mb-2 text-black'>Mô tả sản phẩm</b>
+            <p>{productData.description}</p>
+          </div>
 
-          <hr className='mt-8 sm:w-4/5' />
-          <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+          {/* Chính sách */}
+          <div className='mt-8 text-sm text-gray-700 leading-relaxed  p-4 rounded-lg'>
             <p>✅ 100% Sản phẩm chính hãng</p>
             <p>🚚 Hỗ trợ thanh toán khi nhận hàng</p>
             <p>🔄 Dễ dàng đổi/trả trong vòng 7 ngày</p>
@@ -98,22 +94,10 @@ const Product = () => {
         </div>
       </div>
 
-      {/** Description and review Section */}
-      <div className='mt-20'>
-        <div className='flex'>
-          <b className='border px-5 py-3 text-sm'>Mô tả sản phẩm</b>
-          {/* <b className='border px-5 py-3 text-sm'> Review (122)</b> */}
-        </div>
-        <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
-          <p>{productData.description}</p> {/* Mô tả sản phẩm sẽ được hiển thị ở đây */}
-        </div>
-      </div>
-
-      {/** Display related products */}
+      {/* Display related products */}
       <RelatedProducts category={productData.category} subCategory={productData.subCategory} />
-
     </div>
   ) : <div className='opacity-0'></div>
 }
 
-export default Product
+export default Product;
